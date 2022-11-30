23984a2cbf8750b7e3677638a86863f74fee4400
DShpak27 <mr.shpak@gmail.com>
2022-11-30 10:54:13 +0200

    added simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryParentUlRef = document.querySelector('.gallery');
+// Создали и присвоили значению переменной galleryParentDivRef ссылку на родительский элемент для будущей галереи.
+
+function makeGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}"  />
+                </a>`
+    )
+    .join('');
+}
+
+galleryParentUlRef.innerHTML = makeGalleryItemsMarkup(galleryItems);
+// Присвоили свойству innerHTML родительского элемента галереи результат выполенния функции
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// инициализировали обьект библиотеки
