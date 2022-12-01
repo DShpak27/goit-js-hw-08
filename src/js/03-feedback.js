96d8ee536c529a641be57e6d7289c00098bc7af3
DShpak27 <mr.shpak@gmail.com>
2022-12-01 19:28:53 +0200

    made some fixes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,34 +3,45 @@ import throttle from 'lodash.throttle';
 
 const feedbackFormRef = document.querySelector('.feedback-form');
 const FORM_DATA_STORAGE_KEY = 'formData';
-const unsubmittedFormData = {};
 const restoredFormData = JSON.parse(
   localStorage.getItem(FORM_DATA_STORAGE_KEY)
 );
+let formData = {};
 
 function restoreUnsubmittedInputs() {
   if (restoredFormData) {
     Object.keys(restoredFormData).forEach(key => {
       document.querySelector(`[name=${key}]`).value = restoredFormData[key];
     });
+    formData = JSON.parse(localStorage.getItem(FORM_DATA_STORAGE_KEY));
   }
 }
 
 restoreUnsubmittedInputs();
 
 function onFeedbackFormInput(evt) {
-  unsubmittedFormData[evt.target.name] = evt.target.value;
+  formData[evt.target.name] = evt.target.value;
 
-  localStorage.setItem(
-    FORM_DATA_STORAGE_KEY,
-    JSON.stringify(unsubmittedFormData)
-  );
+  localStorage.setItem(FORM_DATA_STORAGE_KEY, JSON.stringify(formData));
 }
 
-feedbackFormRef.addEventListener('input', throttle(onFeedbackFormInput, 500));
+feedbackFormRef.addEventListener('input', onFeedbackFormInput);
 
 feedbackFormRef.addEventListener('submit', evt => {
   evt.preventDefault();
+
+  let formDataValues = new FormData(feedbackFormRef);
+  for (let inputElement of formDataValues) {
+    if (inputElement.includes('')) {
+      alert('Не все поля заполненны');
+      return;
+    }
+  }
+
+  for (let [name, value] of formDataValues) {
+    console.log(`Вы внесли следующее значение в поле ${name}: ${value}`);
+  }
   localStorage.removeItem(FORM_DATA_STORAGE_KEY);
   evt.currentTarget.reset();
+  formData = {};
 });
