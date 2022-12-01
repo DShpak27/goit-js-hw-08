2aaa707d586e0a9ffcdd86aeb5248f75bf599db0
DShpak27 <mr.shpak@gmail.com>
2022-12-01 02:08:14 +0200

    is done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+// Импортируем функцию throttle из 'lodash.throttle'.
 
+const feedbackFormRef = document.querySelector('.feedback-form');
+const FORM_DATA_STORAGE_KEY = 'formData';
+const unsubmittedFormData = {};
+const restoredFormData = JSON.parse(
+  localStorage.getItem(FORM_DATA_STORAGE_KEY)
+);
+
+function restoreUnsubmittedInputs() {
+  if (restoredFormData) {
+    Object.keys(restoredFormData).forEach(key => {
+      document.querySelector(`[name=${key}]`).value = restoredFormData[key];
+    });
+  }
+}
+
+restoreUnsubmittedInputs();
+
+function onFeedbackFormInput(evt) {
+  unsubmittedFormData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(
+    FORM_DATA_STORAGE_KEY,
+    JSON.stringify(unsubmittedFormData)
+  );
+}
+
+feedbackFormRef.addEventListener('input', throttle(onFeedbackFormInput, 500));
+
+feedbackFormRef.addEventListener('submit', evt => {
+  evt.preventDefault();
+  localStorage.removeItem(FORM_DATA_STORAGE_KEY);
+  evt.currentTarget.reset();
+});
